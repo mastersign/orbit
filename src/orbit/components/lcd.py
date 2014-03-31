ead3b38ab1a5c12ef7817a9e96b07401cd239e1d
Tobias Kiertscher <dev@mastersign.de>
2014-03-31 13:34:27 +0200

    added orbit.components.timer.IntervalTimer, orbit.components.lcd.LCDWatch

--- src/orbit/components/lcd.py
+++ src/orbit/components/lcd.py
@@ -1,6 +1,7 @@
 # Package orbit.components.lcd
 
-from ..application import Component, MultiDeviceHandle
+from datetime import datetime
+from ..application import Component, SingleDeviceHandle, MultiDeviceHandle
 from tinkerforge.bricklet_lcd_20x4 import BrickletLCD20x4
 
 LCD204 = BrickletLCD20x4
@@ -18,9 +19,14 @@ class LCDButtonsComponent(Component):
 		uid = device.get_identity()[0]
 
 		def button_pressed(no):
-			self.send('button', (uid, no))
+			self.send('button_pressed', (uid, no))
 
 		device.register_callback(LCD204.CALLBACK_BUTTON_PRESSED, button_pressed)
+
+		def button_released(no):
+			self.send('button_released', (uid, no))
+
+		device.register_callback(LCD204.CALLBACK_BUTTON_RELEASED, button_released)
 
 
 class LCDBacklightComponent(Component):
@@ -61,3 +67,31 @@ class LCDBacklightComponent(Component):
 	def on_core_started(self):
 		super().on_core_started()
 		self.update_devices()
+
+
+class LCDWatch(Component):
+
+	def __init__(self, core, name, event_info, lcd_uid = None, 
+		lines = {0: "%d.%m.%Y  %H:%M:%S"}):
+
+		super().__init__(core, name)
+		self.lines = lines
+
+		if lcd_uid:
+			self.lcd_handle = SingleDeviceHandle(
+				'lcd', LCD204.DEVICE_IDENTIFIER, uid = lcd_uid)
+		else:
+			self.lcd_handle = MultiDeviceHandle(
+				'lcds', LCD204.DEVICE_IDENTIFIER)
+
+		self.add_device_handle(self.lcd_handle)
+
+		self.listen(event_info.create_listener(self.process_event))
+
+	def process_event(self, sender, name, value):
+		self.lcd_handle.for_all_devices(self.show_time)
+
+	def show_time(self, device):
+		for line in self.lines.keys():
+			text = datetime.now().strftime(self.lines[line])
+			device.write_line(line, 0, text)
--- src/orbit/components/timer.py
+++ src/orbit/components/timer.py
@@ -1,6 +1,8 @@
 # Package orbit.components.timer
 
-from threading import Timer
+from datetime import datetime
+from time import time
+from threading import Timer, Thread
 from ..application import Component
 
 class ActivityTimer(Component):
@@ -46,4 +48,56 @@ class ActivityTimer(Component):
 		if self.timer:
 			self.timer.cancel()
 			self.timer = None
+		super().on_core_stopped()
+
+class IntervalTimer(Component):
+
+	def __init__(self, core, name, initial_state = True, event_info = None, interval = 1):
+		super().__init__(core, name)
+		self.next_call = time()
+		self.interval = interval
+		self.timer = None
+		self.state = False
+		self.initial_state = initial_state
+		if event_info:
+			self.listen(event_info.create_listener(self.process_event))
+
+	def process_event(self, sender, name, value):
+		if value == self.state:
+			return
+		if value:
+			self.start()
+		else:
+			self.stop()
+
+	def start(self, fire = True):
+		self.state = True
+		self.next_call = time()
+		self.timer_callback(fire = fire)
+
+	def stop(self):
+		self.state = False
+		if self.timer:
+			self.timer.cancel()
+			self.timer = None
+
+	def timer_callback(self, fire = True):
+		if fire:
+			self.send('timer', self.next_call)
+		self.next_call = self.next_call + self.interval
+		td = self.next_call - time()
+		if td > 0:
+			self.timer = Timer(td, self.timer_callback)
+			self.timer.start()
+		else:
+			self.trace("overtaken by workload - interval exceeded")
+			Thread(target = self.start).start()
+
+	def on_core_started(self):
+		super().on_core_started()
+		if self.initial_state:
+			self.start(fire = False)
+
+	def on_core_stopped(self):
+		self.stop()
 		super().on_core_stopped()
