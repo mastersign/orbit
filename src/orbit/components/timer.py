71d95789e51aaf976988e247fccd5c4aecc83940
Tobias Kiertscher <dev@mastesign.de>
2014-04-01 16:04:20 +0200

    completed timer class definitions, added initial_state to ActivityTimer

--- src/orbit/components/timer.py
+++ src/orbit/components/timer.py
@@ -7,11 +7,17 @@ from ..application import Component
 
 class ActivityTimer(Component):
 
-	def __init__(self, core, name, event_info, timeout = 6):
-		super().__init__(core, name)
+	def __init__(self, core, name, event_info, 
+		initial_state = True, timeout = 6,
+		tracing = None, event_tracing = None):
+
+		super().__init__(core, name,
+			tracing = tracing, event_tracing = event_tracing)
 		self.timeout = timeout
 		self.timer = None
 		self.state = False
+		self.initial_state = initial_state
+
 		self.listen(event_info.create_listener(self.process_event))
 
 	def process_event(self, sender, name, value):
@@ -42,7 +48,8 @@ class ActivityTimer(Component):
 
 	def on_core_started(self):
 		super().on_core_started()
-		self.trigger()
+		if self.initial_state:
+			self.trigger()
 
 	def on_core_stopped(self):
 		if self.timer:
@@ -52,13 +59,18 @@ class ActivityTimer(Component):
 
 class IntervalTimer(Component):
 
-	def __init__(self, core, name, initial_state = True, event_info = None, interval = 1):
-		super().__init__(core, name)
+	def __init__(self, core, name, event_info = None, 
+		initial_state = True, interval = 1,
+		tracing = None, event_tracing = None):
+
+		super().__init__(core, name,
+			tracing = tracing, event_tracing = event_tracing)
 		self.next_call = time()
 		self.interval = interval
 		self.timer = None
 		self.state = False
 		self.initial_state = initial_state
+
 		if event_info:
 			self.listen(event_info.create_listener(self.process_event))
 
