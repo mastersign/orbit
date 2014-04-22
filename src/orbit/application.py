959fb0e39ff5106f2a85d93fb60c69350415b355
Tobias Kiertscher <dev@mastersign.de>
2014-04-22 15:43:36 +0200

    extracted DeviceManager and Blackboard from Core, prefixed private fields with underscore, introduced global _trace function

--- src/orbit/application.py
+++ src/orbit/application.py
@@ -7,38 +7,25 @@ from . import setup
 from .devices import known_device, get_device_identifier, device_name, device_instance
 from tinkerforge.ip_connection import IPConnection, Error
 
+def _trace(text, source):
+	print(datetime.now().strftime("[%Y-%m-%d %H-%M-%S] ") + ("%s: %s" % (source, text)))
+
 class Core:
 
-	def __init__(self, conf = setup.Configuration(), tracing = False, event_tracing = False):
+	def __init__(self, config = setup.Configuration()):
 
 		self.started = False
-		self.connected = False
-		self.conf = conf
-		self.tracing = tracing
-		self.event_tracing = event_tracing
-		self.devices = {}
-		self.device_callbacks = {}
+		self.configuration = config
 		self.components = {}
-		self.event_listeners = {}
-		self.event_groups = {}
-		self.component_groups = {}
 
-		# initialize IP connection
-		self.conn = IPConnection()
-		self.conn.set_auto_reconnect(True)
-		self.conn.register_callback(IPConnection.CALLBACK_ENUMERATE, self.cb_enumerate)
-		self.conn.register_callback(IPConnection.CALLBACK_CONNECTED, self.cb_connected)
-		self.conn.register_callback(IPConnection.CALLBACK_DISCONNECTED, self.cb_disconnected)
+		self.device_manager = DeviceManager(self)
+		self.blackboard = Blackboard(self)
 
 		self.trace("application core initialized")
 
 	def trace(self, text):
-		if self.tracing:
-			print(datetime.now().strftime("[%Y-%m-%d %H-%M-%S] ") + "Core: " + text)
-
-	def event_trace(self, text):
-		if self.event_tracing:
-			self.trace(text)
+		if self.configuration.core_tracing:
+			_trace(text, 'Core')
 
 	def start(self):
 		if self.started:
@@ -46,31 +33,12 @@ class Core:
 			return
 		self.trace("starting application core")
 
-		def build_group_lookup(names_by_group):
-			groups_by_name = {}
-			for group in names_by_group.keys():
-				names = names_by_group[group]
-				for name in names:
-					if name not in groups_by_name:
-						groups = []
-						groups_by_name[name] = groups
-					else:
-						groups = groups_by_name[name]
-					groups.append(group)
-			return groups_by_name
-
-		self.event_group_lookup = build_group_lookup(self.event_groups)
-		self.component_group_lookup = build_group_lookup(self.component_groups)
-
+		self.blackboard.initialize()
 		self.started = True
+		self.for_each_component(
+			lambda c: c.on_core_started())
+		self.device_manager.start()
 
-		for c in self.components.values():
-			c.on_core_started()
-		if self.conn.get_connection_state() == IPConnection.CONNECTION_STATE_DISCONNECTED:
-			host = self.conf.host
-			port = self.conf.port
-			self.trace("connecting to %s:%d" % (host, port))
-			self.conn.connect(host, port)
 		self.trace("application core started")
 
 	def stop(self):
@@ -78,16 +46,56 @@ class Core:
 			self.trace("application core allready stopped")
 			return
 		self.trace("stopping application core")
-		self.unbind_all_devices()
+
+		self.device_manager.stop()
+		self.started = False
+		self.for_each_component(
+			lambda c: c.on_core_stopped())
+			
+		self.trace("application core stopped")
+
+	def add_component(self, component):
+		# store reference to component
+		self.components[component.name] = component
+
+	def for_each_component(self, f):
+		for component in self.components.values():
+			f(component)
+
+
+class DeviceManager:
+
+	def __init__(self, core):
+		self._core = core
+		self.connected = False
+		self.devices = {}
+		self._device_callbacks = {}
+
+		# initialize IP connection
+		self.conn = IPConnection()
+		self.conn.set_auto_reconnect(True)
+		self.conn.register_callback(IPConnection.CALLBACK_ENUMERATE, self._cb_enumerate)
+		self.conn.register_callback(IPConnection.CALLBACK_CONNECTED, self._cb_connected)
+		self.conn.register_callback(IPConnection.CALLBACK_DISCONNECTED, self._cb_disconnected)
+
+	def trace(self, text):
+		if self._core.configuration.device_tracing:
+			_trace(text, 'DeviceManager')
+
+	def start(self):
+		if self.conn.get_connection_state() == IPConnection.CONNECTION_STATE_DISCONNECTED:
+			host = self._core.configuration.host
+			port = self._core.configuration.port
+			self.trace("connecting to %s:%d" % (host, port))
+			self.conn.connect(host, port)
+
+	def stop(self):
+		self._unbind_all_devices()
 		if self.conn.get_connection_state() != IPConnection.CONNECTION_STATE_DISCONNECTED:
 			self.trace("disconnecting")
 			self.conn.disconnect()
-		self.started = False
-		for c in self.components.values():
-			c.on_core_stopped()
-		self.trace("application core stopped")
 
-	def cb_enumerate(self, uid, connected_uid, position, hardware_version,
+	def _cb_enumerate(self, uid, connected_uid, position, hardware_version,
 	                 firmware_version, device_identifier, enumeration_type):
 
 		if enumeration_type == IPConnection.ENUMERATION_TYPE_AVAILABLE or \
@@ -96,16 +104,16 @@ class Core:
 			self.trace("device present [%s] %s" % (uid, device_name(device_identifier)))
 			if known_device(device_identifier):
 				# bind device and notify components
-				self.bind_device(device_identifier, uid)
+				self._bind_device(device_identifier, uid)
 			else:
 				self.trace("could not create a device binding for device identifier " + device_identifier)
 		if enumeration_type == IPConnection.ENUMERATION_TYPE_DISCONNECTED:
 			# recognize absence of device
 			self.trace("device absent [%s]" % uid)
 			# unbind device and notify components
-			self.unbind_device(uid)
+			self._unbind_device(uid)
 
-	def cb_connected(self, reason):
+	def _cb_connected(self, reason):
 		self.connected = True
 		# recognize connection
 		if reason == IPConnection.CONNECT_REASON_AUTO_RECONNECT:
@@ -113,12 +121,12 @@ class Core:
 		else:
 			self.trace("connection established")
 		# notify components
-		for c in self.components.values():
-			c.on_connected()
+		self._core.for_each_component(
+			lambda c: c.on_connected())
 		# enumerate devices
 		self.conn.enumerate()
 
-	def cb_disconnected(self, reason):
+	def _cb_disconnected(self, reason):
 		self.connected = False
 		# recognize lost connection
 		if reason == IPConnection.DISCONNECT_REASON_ERROR:
@@ -128,10 +136,10 @@ class Core:
 		else:
 			self.trace("connection lost")
 		# notify components
-		for c in self.components.values():
-			c.on_disconnected()
+		self._core.for_each_component(
+			lambda c: c.on_disconnected())
 
-	def bind_device(self, device_identifier, uid):
+	def _bind_device(self, device_identifier, uid):
 		self.trace("binding device [%s]" % uid)
 		# create binding instance
 		device = device_instance(device_identifier, uid, self.conn)
@@ -141,41 +149,41 @@ class Core:
 		# store reference to binding instance
 		self.devices[uid] = device
 		# register callbacks
-		if uid in self.device_callbacks:
-			callbacks = self.device_callbacks[uid]
+		if uid in self._device_callbacks:
+			callbacks = self._device_callbacks[uid]
 			for event in callbacks:
 				self.trace("binding dispatcher to [%s] (%s)" % (uid, event))
 				mcc = callbacks[event]
 				device.register_callback(event, mcc)
 		# notify components
-		for c in self.components.values():
-			c.on_bind_device(device)
+		self._core.for_each_component(
+			lambda c: c.on_bind_device(device))
 
-	def unbind_device(self, uid):
+	def _unbind_device(self, uid):
 		if uid in self.devices:
 			self.trace("unbinding device [%s]" % uid)
 			device = self.devices[uid]
 			# notify components
-			for c in self.components.values():
-				c.on_unbind_device(device)
+			self._core.for_each_component(
+				lambda c: c.on_unbind_device(device))
 			# delete reference to binding interface
 			del(self.devices[uid])
 
 			# delete reference to multicast callbacks
-			if uid in self.device_callbacks:
-				del(self.device_callbacks[uid])
+			if uid in self._device_callbacks:
+				del(self._device_callbacks[uid])
 		else:
 			self.trace("attempt to unbind not bound device [%s]" % uid)
 
-	def unbind_all_devices(self):
+	def _unbind_all_devices(self):
 		for uid in list(self.devices.keys()):
-			self.unbind_device(uid)
+			self._unbind_device(uid)
 
 	def add_device_callback(self, uid, event, callback):
-		if uid not in self.device_callbacks:
-			self.device_callbacks[uid] = {}
+		if uid not in self._device_callbacks:
+			self._device_callbacks[uid] = {}
 		
-		callbacks = self.device_callbacks[uid]
+		callbacks = self._device_callbacks[uid]
 		
 		if event not in callbacks:
 			self.trace("creating dispatcher for [%s] (%s)" % (uid, event))
@@ -191,25 +199,34 @@ class Core:
 		mcc.add_callback(callback)
 
 	def remove_device_callback(self, uid, event, callback):
-		if uid in self.device_callbacks:
-			callbacks = self.device_callbacks[uid]
+		if uid in self._device_callbacks:
+			callbacks = self._device_callbacks[uid]
 			if event in callbacks:
 				mcc = callbacks[event]
 				self.trace("removing callback from dispatcher for [%s] (%s)" % (uid, event))
 				mcc.remove_callback(callback)
 
-	def add_component(self, component):
-		# store reference to component
-		self.components[component.name] = component
+
+class Blackboard:
+
+	def __init__(self, core):
+		self._core = core
+		self._event_listeners = {}
+		self._event_groups = {}
+		self._sender_groups = {}
+
+	def trace(self, text):
+		if self._core.configuration.event_tracing:
+			_trace(text, 'Blackboard')
 
 	def listen(self, event_listener):
 		sender = event_listener.sender
 		name = event_listener.name
-		if sender in self.event_listeners:
-			sender_listeners = self.event_listeners[sender]
+		if sender in self._event_listeners:
+			sender_listeners = self._event_listeners[sender]
 		else:
 			sender_listeners = {}
-			self.event_listeners[sender] = sender_listeners
+			self._event_listeners[sender] = sender_listeners
 		if name in sender_listeners:
 			name_listeners = sender_listeners[name]
 		else:
@@ -217,9 +234,26 @@ class Core:
 			sender_listeners[name] = name_listeners
 		name_listeners.append(event_listener)
 
+	def initialize(self):
+		def build_group_lookup(names_by_group):
+			groups_by_name = {}
+			for group in names_by_group.keys():
+				names = names_by_group[group]
+				for name in names:
+					if name not in groups_by_name:
+						groups = []
+						groups_by_name[name] = groups
+					else:
+						groups = groups_by_name[name]
+					groups.append(group)
+			return groups_by_name
+
+		self.event_group_lookup = build_group_lookup(self._event_groups)
+		self.sender_group_lookup = build_group_lookup(self._sender_groups)
+
 	def send(self, sender, name, value):
-		if not self.started:
-			self.event_trace("DROPPED event before core started (%s, %s)" \
+		if not self._core.started:
+			self.trace("DROPPED event before core started (%s, %s)" \
 				% (sender, name))
 			return
 
@@ -231,8 +265,8 @@ class Core:
 				send_by_name(listeners_by_name, name)
 				send_by_name(listeners_by_name, None)
 
-			if s and s in self.component_group_lookup:
-				for g in self.component_group_lookup[s]:
+			if s and s in self.sender_group_lookup:
+				for g in self.sender_group_lookup[s]:
 					send_by_sender(lookup, g)
 
 		def send_by_name(lookup, n):
@@ -245,37 +279,40 @@ class Core:
 
 		def call_listeners(listeners):
 			for l in listeners:
-				self.event_trace("ROUTE %s, %s => %s (%s, %s)" \
+				self.trace("ROUTE %s, %s => %s (%s, %s)" \
 					% (sender, name, l.component, l.sender, l.name))
 				l(event)
 
-		send_by_sender(self.event_listeners, sender)
-		send_by_sender(self.event_listeners, None)
+		send_by_sender(self._event_listeners, sender)
+		send_by_sender(self._event_listeners, None)
 
 	def event_group(self, group_name, *names):
-		self.event_groups[group_name] = names
+		self._event_groups[group_name] = names
 
-	def component_group(self, group_name, *names):
-		self.component_groups[group_name] = names
+	def sender_group(self, group_name, *names):
+		self._sender_groups[group_name] = names
 
 
 class Component:
 
-	def __init__(self, core, name, tracing = None, event_tracing = None):
-		self.name = name
-		self.tracing = tracing
-		self.event_tracing = event_tracing
-		self.device_handles = []
+	def __init__(self, core, name, 
+		tracing = None, event_tracing = None):
 		self.core = core
+		self.name = name
+		self._tracing = tracing
+		self._event_tracing = event_tracing
+		self._device_handles = []
 		self.core.add_component(self)
 
 	def trace(self, text):
-		if self.tracing != False and (self.tracing or self.core.tracing):
-			print("%s %s: %s" % (datetime.now().strftime("[%Y-%m-%d %H-%M-%S]"), self.name, text))
+		if self._tracing != False and \
+			(self._tracing or self.core.configuration.component_tracing):
+			_trace(text, self.name)
 
 	def event_trace(self, text):
-		if self.event_tracing != False and (self.event_tracing or self.core.event_tracing):
-			self.trace(text)
+		if self._event_tracing != False and \
+			(self._event_tracing or self.core.configuration.event_tracing):
+			_trace(text, self.name)
 
 	def on_core_started(self):
 		# can be overriden in sub classes
@@ -294,39 +331,39 @@ class Component:
 		pass
 
 	def on_bind_device(self, device):
-		for dh in self.device_handles:
+		for dh in self._device_handles:
 			dh.on_bind_device(device)
 
 	def on_unbind_device(self, device):
-		for dh in self.device_handles:
+		for dh in self._device_handles:
 			dh.on_unbind_device(device)
 
 	def add_device_handle(self, device_handle):
-		self.device_handles.append(device_handle)
+		self._device_handles.append(device_handle)
 		device_handle.register_component(self)
 
 	def listen(self, event_listener):
 		event_listener.component = self.name
-		self.core.listen(event_listener)
+		self.core.blackboard.listen(event_listener)
 
 	def send(self, name, value = None):
 		self.event_trace("EVENT %s: %s" % (name, str(value)))
-		self.core.send(self.name, name, value)
+		self.core.blackboard.send(self.name, name, value)
 
 
 class MulticastCallback:
 
 	def __init__(self):
-		self.callbacks = []
+		self._callbacks = []
 
 	def add_callback(self, callback):
-		self.callbacks.append(callback)
+		self._callbacks.append(callback)
 
 	def remove_callback(self, callback):
-		self.callbacks.remove(callback)
+		self._callbacks.remove(callback)
 
 	def __call__(self, *pargs, **nargs):
-		for callback in self.callbacks:
+		for callback in self._callbacks:
 			callback(*pargs, **nargs)
 
 
@@ -334,35 +371,38 @@ class DeviceHandle:
 
 	def __init__(self, name, bind_callback, unbind_callback):
 		self.name = name
-		self.bind_callback = bind_callback
-		self.unbind_callback = unbind_callback
+		self._bind_callback = bind_callback
+		self._unbind_callback = unbind_callback
 		self.devices = []
-		self.callbacks = {}
+		self._callbacks = {}
+		self._component = None
 
 	def register_component(self, component):
-		self.component = component
+		self._component = component
 
 	def on_bind_device(self, device):
 		uid = device.identity[0]
-		self.component.trace("binding device [%s] to handle %s" \
+		self._component.trace("binding device [%s] to handle %s" \
 			% (uid, self.name))
 		self.devices.append(device)
-		for event_code in self.callbacks:
-			self.component.core.add_device_callback(
-					uid, event_code, self.callbacks[event_code])
-		if self.bind_callback:
-			self.bind_callback(device)
+		dm = self._component.core.device_manager
+		for event_code in self._callbacks:
+			dm.add_device_callback(
+					uid, event_code, self._callbacks[event_code])
+		if self._bind_callback:
+			self._bind_callback(device)
 
 	def on_unbind_device(self, device):
 		uid = device.identity[0]
 		if device in self.devices:
-			self.component.trace("unbinding device [%s] from handle %s" \
+			self._component.trace("unbinding device [%s] from handle %s" \
 				% (uid, self.name))
-			if self.unbind_callback:
-				self.unbind_callback(device)
-			for event_code in self.callbacks:
-				self.component.core.remove_device_callback(
-						uid, event_code, self.callbacks[event_code])
+			if self._unbind_callback:
+				self._unbind_callback(device)
+			dm = self._component.core.device_manager
+			for event_code in self._callbacks:
+				dm.remove_device_callback(
+					uid, event_code, self._callbacks[event_code])
 			self.devices.remove(device)
 
 	def for_each_device(self, f):
@@ -375,20 +415,22 @@ class DeviceHandle:
 
 	def register_callback(self, event_code, callback):
 		self.unregister_callback(event_code)
-		self.callbacks[event_code] = callback
+		self._callbacks[event_code] = callback
+		dm = self._component.core.device_manager
 		self.for_each_device(
 			lambda device:
-				self.component.core.add_device_callback(
+				dm.add_device_callback(
 					device.identity[0], event_code, callback))
 
 	def unregister_callback(self, event_code):
-		if event_code in self.callbacks:
-			callback = self.callbacks[event_code]
+		if event_code in self._callbacks:
+			callback = self._callbacks[event_code]
+			dm = self._component.core.device_manager
 			self.for_each_device(
 				lambda device: 
-					self.component.core.remove_device_callback(
+					dm.remove_device_callback(
 						device.identity[0], event_code, callback))
-			del(self.callbacks[event_code])
+			del(self._callbacks[event_code])
 
 
 class SingleDeviceHandle(DeviceHandle):
