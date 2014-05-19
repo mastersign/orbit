67638c91fae7f08e3f8542e534a4ddbfea071425
Tobias Kiertscher <dev@mastersign.de>
2014-05-19 11:49:35 +0200

    added orbit.jobs.apps.EscapableApp

--- src/orbit/jobs/apps.py
+++ src/orbit/jobs/apps.py
@@ -2,9 +2,25 @@
 
 from ..application import App, Slot
 from ..components.timer import IntervalTimerComponent
-from ..components.lcd import LCDButtonsComponent, LCDWatchComponent
+from ..components.lcd import LCDButtonsComponent, LCDWatchComponent, LCDMessageComponent
 
-class WatchApp(App):
+class EscapableApp(App):
+
+	def __init__(self, *args, **nargs):
+		super().__init__(*args, **nargs)
+
+		self.add_component(
+			LCDButtonsComponent('lcd_buttons'))
+
+		self.add_deactivator(Slot(self.name, 'lcd_buttons', 'button_pressed',
+			predicate = self._escape_predicate))
+
+	def _escape_predicate(self, job, component, name, value):
+		uid, button = value
+		return button == 0
+
+
+class WatchApp(EscapableApp):
 
 	def __init__(self, name, **nargs):
 		super().__init__(name, **nargs)
@@ -17,12 +33,5 @@ class WatchApp(App):
 				lines = {1: "     %d.%m.%Y     ", 2: "      %H:%M:%S      "},
 				slot = Slot(self.name, 'watch_timer', 'timer')))
 
-		self.add_component(
-			LCDButtonsComponent('lcd_buttons'))
 
-		self.add_deactivator(Slot(self.name, 'lcd_buttons', 'button_pressed',
-			predicate = self._exit_predicate))
 
-	def _exit_predicate(self, job, component, name, value):
-		uid, button = value
-		return button == 0
