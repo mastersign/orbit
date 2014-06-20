7e5e5353f7d12fe464601b0873ab58b4fab57452
Tobias Kiertscher <dev@mastersign.de>
2014-06-20 15:12:36 +0200

    added documentation to common components

--- src/orbit/components/common.py
+++ src/orbit/components/common.py
@@ -2,9 +2,30 @@
 
 # Package orbit.components.common
 
+"""
+Dieses Modul enthält einige Komponenten für den allgemeinen Einsatz.
+
+Enthalten sind die folgenden Komponenten:
+
+- :py:class:`EventCallbackComponent`
+"""
+
 from .. import Component
 
 class EventCallbackComponent(Component):
+	"""
+	Diese Komponente wartet mit Hilfe eines Empfangsmusters auf Nachrichten,
+	und ruft ein Callback auf, wenn eine passende Nachricht eintrifft.
+
+	**Parameter**
+
+	``name``
+		Der Name der Komponente.
+	``slot``
+		Das Empfangsmuster für den Empfang der Nachrichten.
+	``callback``
+		Eine parameterlose Funktion.
+	"""
 
 	def __init__(self, name, 
 		slot, callback):
