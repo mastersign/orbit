2946ba574640535b90ad1d38b0693b671a6c6835
Tobias Kiertscher <dev@mastersign.de>
2014-07-26 12:24:46 +0200

    updated package setup.py with dependency to tinkerforge package

--- src/setup.py
+++ src/setup.py
@@ -4,17 +4,22 @@ from setuptools import setup
 
 setup(
   name='orbit_framework',
-  version='0.1',
+  version='0.1.0',
   description='Ein Python-Framework für robuste TinkerForge-Anwendungen',
   author='Tobias Kiertscher',
   author_email='dev@mastersign.de',
   url='http://mastersign.github.io/orbit/',
+  download_url='https://github.com/mastersign/orbit/releases/download/v0.1.0-alpha/orbit_framework.egg',
   packages=[
     'orbit_framework', 
     'orbit_framework.components',
     'orbit_framework.jobs',
     ],
-    long_description="""\
+  provides='orbit_framework',
+  requires=[
+    'tinkerforge (>=2.1.0)'
+    ],
+  long_description="""\
 ORBIT ermöglicht die Entwicklung von dialogbasierten Systemen \
 durch ein einfaches Konzept von Apps und Services. \
 Es unterstützt eine robuste Anwendungsarchitektur durch wiederverwendbare \
@@ -23,18 +28,18 @@ Nachrichtensystem. Die Verwaltung der Verbindung zu TinkerForge-Bricks \
 und -Bricklets übernimmt ein Gerätemanager, dadurch wird das Entwickeln \
 von Anwendungen, die mit Verbindungsabbrüchen umgehen müssen, stark vereinfacht. \
 """,
-    classifiers=[
-      "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-      "Programming Language :: Python",
-      "Development Status :: 3 - Alpha",
-      "Intended Audience :: Developers",
-      "Natural Language :: German",
-      "Topic :: Home Automation",
-      "Topic :: Games/Entertainment",
-      ],
-    keywords='TinkerForge framework ',
-    license='LGPL-3.0',
-    install_requires=[
-      'setuptools',
-      ],
-    )
+  classifiers=[
+    "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+    "Programming Language :: Python",
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Natural Language :: German",
+    "Topic :: Home Automation",
+    "Topic :: Games/Entertainment",
+    ],
+  keywords='TinkerForge framework',
+  license='LGPL-3.0',
+  install_requires=[
+    'setuptools',
+    ],
+)
