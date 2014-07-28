539586bb80d877fd2621c06d94fbc73d4ef123f1
Tobias Kiertscher <dev@mastersign.de>
2014-07-28 11:40:48 +0200

    improving setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,33 +1,22 @@
 # coding=utf-8
 
-from setuptools import setup
+from setuptools import setup, find_packages
+from os import path
+from codecs import open
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
   name='orbit_framework',
-  version='0.1.0a1',
+  version='0.1.0a',
   description='Ein Python-Framework für robuste TinkerForge-Anwendungen',
   author='Tobias Kiertscher',
   author_email='dev@mastersign.de',
   url='http://mastersign.github.io/orbit/',
-  download_url='https://github.com/mastersign/orbit/releases/download/v0.1.0-alpha/orbit_framework.egg',
-  packages=[
-    'orbit_framework', 
-    'orbit_framework.components',
-    'orbit_framework.jobs',
-    ],
-  provides=['orbit_framework'],
-  requires=[
-    'tinkerforge (>=2.1.0)'
-    ],
-  long_description="""\
-ORBIT ermöglicht die Entwicklung von dialogbasierten Systemen \
-durch ein einfaches Konzept von Apps und Services. \
-Es unterstützt eine robuste Anwendungsarchitektur durch wiederverwendbare \
-Komponenten und die Entkopplung von Anwendungsteilen mit Hilfe eines \
-Nachrichtensystem. Die Verwaltung der Verbindung zu TinkerForge-Bricks \
-und -Bricklets übernimmt ein Gerätemanager, dadurch wird das Entwickeln \
-von Anwendungen, die mit Verbindungsabbrüchen umgehen müssen, stark vereinfacht. \
-""",
+  packages=find_packages(),
+  long_description=long_description,
   classifiers=[
     "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
     "Programming Language :: Python",
@@ -40,7 +29,6 @@ von Anwendungen, die mit Verbindungsabbrüchen umgehen müssen, stark vereinfach
   keywords='TinkerForge framework',
   license='LGPL-3.0',
   install_requires=[
-    'setuptools',
-    'tinkerforge>=2.1.0',
+    'tinkerforge >= 2.1.0',
     ],
 )
